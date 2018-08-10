6d99591143adf8599ea78127f6d0708d100196b6
JiwooHong <tech_jiwoo@woowahan.com>
2018-08-10 15:21:46 +0900

    * add : cart controller, cart controller acceptance test

---
+++ src/main/java/codesquad/web/CartController.java
@@ -0,0 +1,13 @@
+package codesquad.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CartController {
+    @GetMapping("/cart")
+    public String cart(Model model) {
+        return "cart";
+    }
+}
---
+++ src/test/java/codesquad/web/CartControllerAcceptanceTest.java
@@ -0,0 +1,19 @@
+package codesquad.web;
+
+import codesquad.support.test.AcceptanceTest;
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CartControllerAcceptanceTest extends AcceptanceTest {
+
+    @Test
+    public void cart() {
+        ResponseEntity<String> responseEntity = template().getForEntity("/cart", String.class);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        // TODO : fix NPE
+        //assertThat(responseEntity.getHeaders().getLocation().getPath()).contains("cart");
+    }
+}
