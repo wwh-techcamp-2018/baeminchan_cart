7bfb3d394e017cb76f0583d3d090ab50cc7376ba
minsungryu <ryums0227@gmail.com>
2018-08-11 23:48:17 +0900

    del: 사용하지 않는 fetchManager 제거

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -5,18 +5,3 @@ function $(selector) {
 function $_value(selector) {
     return $(selector).value;
 }
-
-function fetchManager({url, method, body, headers, callback}) {
-    fetch(url, {method, body, headers, credentials: "same-origin"})
-        .then((response) => {
-            const value = response;
-            if(value.status == 200) {
-                return;
-            } else {
-                return value.json();
-            }
-
-        }).then((result) => {
-            callback(result);
-    });
-}
