447f4c9ef60b24c0c102528014aa49eccb3ead44
Nooheat <nooheat1228@gmail.com>
2018-08-12 04:09:06 +0900

    feat: Implements toaster with opacity effect
    - Modularize(separate) ShoppingCartSideBar as cart.js to use in product.html view

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -1,28 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
-    new ShoppingCart();
-})
-
-class ShoppingCart {
+class ShoppingCartSideBar {
     constructor() {
-        this.countUpBtns = $_all('.prds_lst_info .up');
-        this.countDownBtns = $_all('.prds_lst_info .down');
-        this.addToCartBtns = $_all('.prds_lst_info button');
         this.noItemAddedLabel = $('.top_box_number > span');
         this.totalItemKindLabel = $('#basket-counter');
-        this.registerEvents();
-        this.initializeCart();
-    }
-
-    initializeCart() {
-        fetchManager({
-            url: '/api/cart/size',
-            method: 'GET',
-            onSuccess: ({json}) => {
-                if(json.totalKind >= 1) {
-                    this.fetchCurrentItemCountLabel(json.totalKind);
-                }
-            }
-        })
+        this.basketToaster = $('#basket-toaster');
     }
 
     fetchCurrentItemCountLabel(totalKind) {
@@ -30,49 +10,11 @@ class ShoppingCart {
         $('#cart_display_exist').style.display = 'block';
         this.totalItemKindLabel.innerText = totalKind;
     }
-    registerEvents() {
-        this.registerCountUpBtnEvent();
-        this.registerCountDownBtnEvent();
-        this.registerAddToCartBtnClickEvent();
-    }
 
-    registerAddToCartBtnClickEvent() {
-        this.addToCartBtns.forEach(e => e.addEventListener('click', (evt) => {
-            console.log(evt.target.getAttribute('data-id'))
-            console.log(this.getBuyCountLabel(evt.target).value)
-            fetchManager({
-                url: `/api/cart`,
-                method: 'POST',
-                headers: {
-                    'content-type': 'application/json'
-                },
-                body: JSON.stringify({
-                    productId: evt.target.getAttribute('data-id'),
-                    count: this.getBuyCountLabel(evt.target).value
-                }),
-                onSuccess: ({json}) => {
-                    console.log(json);
-                    this.fetchCurrentItemCountLabel(json.totalKind);
-                }
-            });
-        }))
-    }
-
-    getBuyCountLabel(element) {
-        return element.closest('dd').querySelector('input.buy_cnt');
-    }
-
-    registerCountUpBtnEvent() {
-        this.countUpBtns.forEach(e => e.addEventListener('click', (evt) => {
-            this.getBuyCountLabel(evt.target).value++;
-        }))
-    }
-
-    registerCountDownBtnEvent() {
-        this.countDownBtns.forEach(e => e.addEventListener('click', (evt) => {
-            const label = this.getBuyCountLabel(evt.target);
-            if (label.value == 1) return;
-            evt.target.closest('.prd_account').querySelector('.buy_cnt').value--;
-        }))
+    notifyItemAdded(title) {
+        clearTimeout(this.currentTimeout);
+        this.basketToaster.querySelector('span').innerText = title;
+        this.basketToaster.classList.add('show');
+        this.currentTimeout = setTimeout(() => this.basketToaster.classList.remove('show'), 1500);
     }
 }
