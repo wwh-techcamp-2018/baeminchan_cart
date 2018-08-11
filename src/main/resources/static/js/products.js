bf34b3baaa0d29c3cd83d6e37bd2b14b1f1555e3
JiwooHong <tech_jiwoo@woowahan.com>
2018-08-12 05:24:31 +0900

    * mod : 장바구니 담기 버튼 누르면 요청보내고 응답에 따라 장바구니에 몇개의 품목이 담겼는지 표시 - 담기(/api/cart/item), 품목갯수(/api/cart/item/count)와 같이 2번의 요청을 보냄

---
+++ src/main/resources/static/js/products.js
@@ -0,0 +1,60 @@
+
+function onDOMContentLoaded() {
+    const addToCartButtons = document.querySelectorAll("#products .btn.cart");
+    addToCartButtons.forEach((button) => { button.addEventListener("click", addToCart) });
+}
+
+function addToCart({ target }) {
+    const productLi = target.closest("li");
+    const url = productLi.querySelector(".prd_tlt > a").href;
+
+    const productId = url.split("/").pop();
+    const quantity = productLi.querySelector("input.buy_cnt").value;
+
+    const dto = {
+        "productId": productId,
+        "quantity": quantity
+    };
+
+    fetchManager({
+        url: "/api/cart/item",
+        method: "POST",
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify(dto),
+        callback: onAddToCart
+    });
+}
+
+function onAddToCart() {
+    fetchManager({
+        url: "/api/cart/item/count",
+        method: "GET",
+        callback: onCartItemCount
+    });
+}
+
+function onCartItemCount(result) {
+    result.json().then(count => {
+        if (count > 0) {
+            showCartItemCount(count);
+        } else {
+            hideCartItemCount();
+        }
+    });
+    // TODO : animation
+}
+
+function showCartItemCount(count) {
+    $("#basket-counter").innerHTML = count;
+    $("#cart_display_exist").style.display = "block";
+    $("#cart_display_none").style.display = "none";
+}
+
+const MIN_CART_ITEM_COUNT = 0;
+function hideCartItemCount() {
+    $_value("#basket-counter").innerHTML = MIN_CART_ITEM_COUNT;
+    $("#cart_display_exist").style.display = "none";
+    $("#cart_display_none").style.display = "block";
+}
+
+document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
