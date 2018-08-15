b1269759941b2ea8b3c520339bb096dc43e7b781
minsungryu <ryums0227@gmail.com>
2018-08-16 00:07:08 +0900

    mod: RestController의 응답방식 변경

--- src/main/java/codesquad/web/ApiCartController.java
+++ src/main/java/codesquad/web/ApiCartController.java
@@ -7,7 +7,7 @@ import codesquad.dto.CartProductDTO;
 import codesquad.service.ProductService;
 import codesquad.util.SessionCart;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,45 +21,50 @@ public class ApiCartController {
     private ProductService productService;
 
     @GetMapping
-    public ResponseEntity<RestResponse> getCartList(@SessionCart Cart cart) {
+    @ResponseStatus(HttpStatus.OK)
+    public RestResponse getCartList(@SessionCart Cart cart) {
         List<CartProductDTO> cartProducts = productService.findAll(cart.getProductIds()).stream()
                 .map(product -> new CartProductDTO(product, cart.getProductCount(product.getId())))
                 .collect(Collectors.toList());
-        return ResponseEntity.ok(new RestResponse(new CartDTO(cartProducts)));
+        return new RestResponse(new CartDTO(cartProducts));
     }
 
     @GetMapping("/products/count")
-    public ResponseEntity<RestResponse> getCartCount(@SessionCart Cart cart) {
-        return ResponseEntity.ok(new RestResponse(cart.getProducts().keySet().size()));
+    @ResponseStatus(HttpStatus.OK)
+    public RestResponse getCartCount(@SessionCart Cart cart) {
+        return new RestResponse(cart.getProducts().keySet().size());
     }
 
     @GetMapping("/products/{id}")
-    public ResponseEntity<RestResponse> getProductDetail(@PathVariable Long id, @SessionCart Cart cart) {
+    @ResponseStatus(HttpStatus.OK)
+    public RestResponse getProductDetail(@PathVariable Long id, @SessionCart Cart cart) {
         Product product = productService.findById(id);
         Integer count = cart.getProductCount(id);
-        return ResponseEntity.ok(new RestResponse(new CartProductDTO(product, count)));
+        return new RestResponse(new CartProductDTO(product, count));
     }
 
     @PostMapping("/products/{id}")
-    public ResponseEntity<RestResponse> addProduct(@PathVariable Long id,
-                                                   @RequestParam(value = "count", required = false) Integer count,
-                                                   @SessionCart Cart cart) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public RestResponse addProduct(@PathVariable Long id,
+                                   @RequestParam(value = "count", required = false) Integer count,
+                                   @SessionCart Cart cart) {
         cart.addProduct(id, count);
         return getCartCount(cart);
     }
 
     @PutMapping("/products/{id}")
-    public ResponseEntity<RestResponse> updateProduct(@PathVariable Long id,
-                                                      @RequestParam(value = "count") Integer count,
-                                                      @SessionCart Cart cart) {
+    @ResponseStatus(HttpStatus.OK)
+    public RestResponse updateProduct(@PathVariable Long id,
+                                      @RequestParam(value = "count") Integer count,
+                                      @SessionCart Cart cart) {
         cart.updateProduct(id, count);
         return getCartCount(cart);
     }
 
     @DeleteMapping("/products/{id}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable Long id, @SessionCart Cart cart) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProduct(@PathVariable Long id, @SessionCart Cart cart) {
         cart.deleteProduct(id);
-        return ResponseEntity.noContent().build();
     }
 
 }
