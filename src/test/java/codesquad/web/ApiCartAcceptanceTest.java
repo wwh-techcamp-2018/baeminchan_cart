985e45ba128ea56d504f126e2515f9d629c35c91
hyukjin-lee <gogokow27@gmail.com>
2018-08-19 19:35:40 +0900

    *test* : ApiCartAcceptanceTest refactoring

--- src/test/java/codesquad/web/ApiCartAcceptanceTest.java
+++ src/test/java/codesquad/web/ApiCartAcceptanceTest.java
@@ -1,34 +1,30 @@
 package codesquad.web;
 
-import codesquad.domain.Cart;
+import codesquad.dto.CartItemDTO;
 import codesquad.support.test.AcceptanceTest;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import javax.servlet.http.HttpSession;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 public class ApiCartAcceptanceTest extends AcceptanceTest {
     @Test
-    public void save(){
-        Cart cart = Cart.builder()
-                .title("집밥 김치찌개")
-                .description("맛있음")
-                .imgUrl("www.naver.com")
-                .price(10000)
+    public void save() {
+        CartItemDTO cartItemDTO = CartItemDTO.builder()
+                .productId(1L)
+                .quantity(1)
                 .build();
-        ResponseEntity<Void> response = template().postForEntity("/products/cart", cart, Void.class);
+        ResponseEntity<Void> response = template().postForEntity("/api/products/cart", cartItemDTO, Void.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
-
-    @Test
-    public void count(){
-        ResponseEntity<Integer> response = template().getForEntity("/products/cart/count", Integer.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).isNotNull();
-    }
+//
+//    @Test
+//    public void count(){
+//        ResponseEntity<Integer> response = template().getForEntity("/products/cart/count", Integer.class);
+//        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+//        assertThat(response.getBody()).isNotNull();
+//    }
 }
